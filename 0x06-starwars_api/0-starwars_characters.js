293d2e7427793e35070ebece5b11413feebed927
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-08 18:20:28 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
 
 const request = require("request")
+const fs = require("fs")
+const { json } = require("stream/consumers")
+argv = process.argv[2]
 
-request.get(`https://swapi-api.alx-tools.com/api/films/3/`)
-.on("response", function(response){
-	console.log(response.statusCode)
-});
+request(`https://swapi-api.alx-tools.com/api/films/${argv}/`, function (error, response, body){
+	json_body = JSON.parse(body)
+	for( i in json_body.characters){
+		console.log(json_body.characters[i]);
+	}
+})
+
+// console.log(req)
