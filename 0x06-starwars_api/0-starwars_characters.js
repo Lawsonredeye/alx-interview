66b1af1ce3607d4507964e9f713a84c5702e0edd
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-08 10:19:06 -0700

    (mod): called the request twice to be able to get each names based on the array fro the api data

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,8 +1,19 @@
 #!/usr/bin/node
 
-const request = require("request")
+const request = require("request");
+const argv = process.argv[2];
 
-request.get(`https://swapi-api.alx-tools.com/api/films/3/`)
-.on("response", function(response){
-	console.log(response.statusCode)
+request(`https://swapi-api.alx-tools.com/api/films/${argv}/`, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const jsonBody = JSON.parse(body).characters;
+  for(const i in jsonBody) {
+    request(`${jsonBody[i]}`, function (err, res, resBody) {
+      if (err) {
+        console.error(err);
+}
+      console.log(JSON.parse(resBody).name);
+    });
+  }
 });
