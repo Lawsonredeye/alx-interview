dd9b9dfd790c391ed51fecf7aed69fa67df3bfc1
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-11 17:21:38 +0100

    using promisees and map to handle new requests

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -22,27 +22,18 @@ request(`https://swapi-api.alx-tools.com/api/films/${argv}`, function (error, re
   if (error) {
     console.error(error);
   } else {
-    const characters = JSON.parse(body).characters;
-    // use promises to be able to print values one at a time
+    const jsonBody = JSON.parse(body).characters;
 
-    const promises = characters.map(url => {
+    let promises = jsonBody.map(url => {
       return new Promise((resolve, reject) => {
-        request(url, function (err, res, resBody) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(resBody.name));
-          }
+        request(url, (err, res, resBody) => {
+          if (err) reject(err);
+          else resolve(JSON.parse(resBody).name);
         });
       });
     });
-    // Resolve all promises sent using resolve
     Promise.all(promises).then(names => {
-      names.forEach(name => {
-        console.log(name);
-      });
-    }).catch(err => {
-      console.error(err);
-    });
+      names.forEach(name => console.log(name));
+    }).catch(error => console.log(error));
   }
 });
