7a78fd15716c4ca6b1360c48f8c01437d60e18da
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-30 00:15:17 +0100

    pycodestyle fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env
+#!/usr/bin/python3
 """function def island_perimeter(grid): that returns the
 perimeter of the island described in grid
 """
+
 
 def island_perimeter(grid):
     """handles perimeter calculaions within a loop
@@ -12,16 +13,16 @@ def island_perimeter(grid):
         for j in range(len(grid)):
             if grid[i][j] == 1:
 
-                if j + 1>= len(grid[i]) or grid[i][j + 1] == 0:
+                if j + 1 >= len(grid[i]) or grid[i][j + 1] == 0:
                     perimeter += 1
-                
+
                 if j - 1 < 0 or grid[i][j + 1] == 0:
                     perimeter += 1
-                
+
                 if i + 1 >= len(grid) or grid[i + 1][j] == 0:
                     perimeter += 1
 
                 if i - 1 < 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-    
+
     return perimeter
