8d22e507077cb6ca3b578111b2978a15e37368d7
Lawsonredeye <omoregbeeolawson@gmail.com>
2024-08-30 00:13:49 +0100

    interview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,10 +1,27 @@
-grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
+#!/usr/bin/env
+"""function def island_perimeter(grid): that returns the
+perimeter of the island described in grid
+"""
 
-print(grid
-      )
+def island_perimeter(grid):
+    """handles perimeter calculaions within a loop
+    to retain its value
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 1:
+
+                if j + 1>= len(grid[i]) or grid[i][j + 1] == 0:
+                    perimeter += 1
+                
+                if j - 1 < 0 or grid[i][j + 1] == 0:
+                    perimeter += 1
+                
+                if i + 1 >= len(grid) or grid[i + 1][j] == 0:
+                    perimeter += 1
+
+                if i - 1 < 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+    
+    return perimeter
